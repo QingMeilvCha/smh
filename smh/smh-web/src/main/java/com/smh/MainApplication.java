c92e67e51aa1d63c743dbf5613881977a315b811
zhouyuhang <zhouyuhang@shein.com>
2018-12-12 10:11:25 +0800

    1

---
+++ smh/smh-web/src/main/java/com/smh/MainApplication.java
@@ -0,0 +1,14 @@
+package com.smh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dell on 2018/12/12.
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
