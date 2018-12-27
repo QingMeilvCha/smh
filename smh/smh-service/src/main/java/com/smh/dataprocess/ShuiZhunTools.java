20477ab4e1d224dc7a1046399dc63d96385e1db9
zhouyuhang <zhouyuhang@shein.com>
2018-12-27 17:36:01 +0800

    数据处理待完成

--- smh/smh-service/src/main/java/com/smh/dataprocess/ShuiZhunTools.java
+++ smh/smh-service/src/main/java/com/smh/dataprocess/ShuiZhunTools.java
@@ -1,8 +1,10 @@
 package com.smh.dataprocess;
 
+import com.smh.sys.SysConstants;
 import com.smh.taskdata.model.TaskDataEntity;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -24,7 +26,18 @@ public class ShuiZhunTools {
             entity.setPreDistance(entity.getPreDistanceUo().subtract(entity.getPreDistanceBottm()));
             entity.setDistanceReduce(entity.getPostDistance().subtract(entity.getPreDistance()));
 
-            //未完成
+            //黑面差值
+            BigDecimal value16 = entity.getPostBlack().subtract(entity.getPreBlack());
+            //红面差值
+            BigDecimal value17 = entity.getPostRed().subtract(entity.getPreRed());
+
+            BigDecimal value10=entity.getPostBlack().add(new BigDecimal(SysConstants.KValue.k1)).subtract(entity.getPostRed());
+            BigDecimal value9=entity.getPreBlack().add(new BigDecimal(SysConstants.KValue.k1)).subtract(entity.getPreRed());
+
+            BigDecimal value11=value10.subtract(value9);
+
+            //待开发
+
         });
         return taskDataEntities;
     }
